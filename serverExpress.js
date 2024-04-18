8c98ddce0fabc3d5fcd5f98cadfb9256abf8b51b
mcarena <mcarena.quijada@outlook.com>
2024-04-18 16:52:58 -0400

    Se instala nodemon para levantar el servidor y se añade la ruta principál que contiene el index.html

--- serverExpress.js
+++ serverExpress.js
@@ -3,6 +3,15 @@ const app = express();
 const PORT = 3000;
 
 
+
 app.listen(PORT, () => {
     console.log(`Servidor en http://localhost:${PORT}`);
+});
+
+// Middleware 
+app.use(express.json());
+app.use(express.static('index.html'));
+// Ruta principal para levantar el html
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
